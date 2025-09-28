1d04f8680d7e7b3f1084a54605729632911c9ccd
MarcusGrass <marcus.grass@protonmail.com>
2025-09-28 13:22:55 +0200

    fix: reorder ci steps for fewer rebuilds

--- assets/testing/ci.sh
+++ assets/testing/ci.sh
@@ -2,7 +2,7 @@
 set -e
 cargo fmt --all --check
 cargo clippy --all-targets --all-features -- -D warnings
-cargo test --all-features
 cargo r -p p2proxy-test-runner
+cargo test --all-features
 cargo deny check
 gitleaks --verbose detect
